ce2f69d7e8c1268e0c3b2366549a2d2d9f66693e
Ruben <rubns.ruiz@gmail.com>
2023-06-18 15:30:25 -0300

    final working game and updated gitignore

---
+++ data.py
@@ -0,0 +1,112 @@
+logo = """
+
+                                                                                                                                                                         
+8888888 8888888888 8 8888     ,o888888o.             8888888 8888888888   .8.           ,o888888o.             8888888 8888888888 ,o888888o.     8 8888888888            
+      8 8888       8 8888    8888     `88.                 8 8888        .888.         8888     `88.                 8 8888    . 8888     `88.   8 8888                  
+      8 8888       8 8888 ,8 8888       `8.                8 8888       :88888.     ,8 8888       `8.                8 8888   ,8 8888       `8b  8 8888                  
+      8 8888       8 8888 88 8888                          8 8888      . `88888.    88 8888                          8 8888   88 8888        `8b 8 8888                  
+      8 8888       8 8888 88 8888                          8 8888     .8. `88888.   88 8888                          8 8888   88 8888         88 8 888888888888          
+      8 8888       8 8888 88 8888                          8 8888    .8`8. `88888.  88 8888                          8 8888   88 8888         88 8 8888                  
+      8 8888       8 8888 88 8888                          8 8888   .8' `8. `88888. 88 8888                          8 8888   88 8888        ,8P 8 8888                  
+      8 8888       8 8888 `8 8888       .8'                8 8888  .8'   `8. `88888.`8 8888       .8'                8 8888   `8 8888       ,8P  8 8888                  
+      8 8888       8 8888    8888     ,88'                 8 8888 .888888888. `88888.  8888     ,88'                 8 8888    ` 8888     ,88'   8 8888                  
+      8 8888       8 8888     `8888888P'                   8 8888.8'       `8. `88888.  `8888888P'                   8 8888       `8888888P'     8 888888888888          
+
+                                             
+"""
+
+board = [[" ", " ", " "], [" ", " ", " "], [" ", " ", " "]]
+
+
+# Clean board for new game
+def clean_board(board):
+    board[0] = [" ", " ", " "]
+    board[1] = [" ", " ", " "]
+    board[2] = [" ", " ", " "]
+
+
+# Check if the current player just won
+def is_winner(current_player):
+    # First we check columns and rows
+    for i in range(3):
+        if (
+            board[i][0] == current_player
+            and board[i][1] == current_player
+            and board[i][2] == current_player
+        ):
+            return True
+        elif (
+            board[0][i] == current_player
+            and board[1][i] == current_player
+            and board[2][i] == current_player
+        ):
+            return True
+        else:
+            pass
+    # Then we check the two diagonals
+    if (
+        board[0][0] == current_player
+        and board[1][1] == current_player
+        and board[2][2] == current_player
+    ):
+        return True
+    elif (
+        board[0][2] == current_player
+        and board[1][1] == current_player
+        and board[2][0] == current_player
+    ):
+        return True
+    else:
+        return False
+
+
+# Check if the board is already full
+def is_board_full():
+    if (
+        board[0][0] != " "
+        and board[0][1] != " "
+        and board[0][2] != " "
+        and board[1][0] != " "
+        and board[1][1] != " "
+        and board[1][2] != " "
+        and board[2][0] != " "
+        and board[2][1] != " "
+        and board[2][2] != " "
+    ):
+        return True
+
+
+# # Winning rows
+# board[0][0]
+# board[0][1]
+# board[0][2]
+
+# board[1][0]
+# board[1][1]
+# board[1][2]
+
+# board[2][0]
+# board[2][1]
+# board[2][2]
+
+# # Winnings columns
+# board[0][0]
+# board[1][0]
+# board[2][0]
+
+# board[0][1]
+# board[1][1]
+# board[2][1]
+
+# board[0][2]
+# board[1][2]
+# board[2][2]
+
+# #Winnings diagonals
+# board[0][0]
+# board[1][1]
+# board[2][2]
+
+# board[0][2]
+# board[1][1]
+# board[2][0]
--- main.py
+++ main.py
@@ -1,13 +1,99 @@
-game = [[" ", " ", " "], [" ", " ", " "], [" ", " ", " "]]
+from data import logo, board, is_winner, is_board_full, clean_board
+import random
 
-current_board = f"""
-+---+---+---+
-| {game[0][0]} | {game[0][1]} | {game[0][2]} |
-+---+---+---+
-| {game[1][0]} | {game[1][1]} | {game[1][2]} |
-+---+---+---+
-| {game[2][0]} | {game[2][1]} | {game[2][2]} |
-+---+---+---+
-"""
 
-print(current_board)
+def current_board(board):
+    return f"""
+    +---+---+---+
+    | {board[0][0]} | {board[0][1]} | {board[0][2]} |
+    +---+---+---+
+    | {board[1][0]} | {board[1][1]} | {board[1][2]} |
+    +---+---+---+
+    | {board[2][0]} | {board[2][1]} | {board[2][2]} |
+    +---+---+---+
+    """
+
+
+players = [{"name": "Player X", "symbol": "X"}, {"name": "Player O", "symbol": "O"}]
+
+
+def game():
+    clean_board(board)
+    print(
+        f"""{logo}
+        Welcome to a new Tic Tac Toe Game!
+        Here's your brand new board, ready for an exciting game:
+        {current_board(board)}
+        """
+    )
+
+    # First Move
+    current_player = random.choice(players)
+    current_move = input(
+        f"""{current_player['name']} will be the first to move.
+        Come on {current_player['name']}, choose a place for your {current_player['symbol']}
+        (Input a row and a column, separated by a space):
+        """
+    )
+
+    current_move_row = int(current_move.split(" ")[0])
+    current_move_column = int(current_move.split(" ")[1])
+
+    board[current_move_row][current_move_column] = current_player["symbol"]
+    print(current_board(board))
+
+    # Next Moves
+    while True:
+        # Switch players
+        if current_player == players[0]:
+            current_player = players[1]
+        else:
+            current_player = players[0]
+
+        # Input next move
+        current_move = input(
+            f"""
+            Now it's {current_player['name']}'s turn.
+            What's your move? (row column):
+            """
+        )
+        current_move_row = int(current_move.split(" ")[0])
+        current_move_column = int(current_move.split(" ")[1])
+        # Check if input cell is taken
+        while board[current_move_row][current_move_column] != " ":
+            current_move = input(
+                f"""
+            Sorry {current_player['name']}, but that cell is taken.
+            Try a different move? (row column):
+            """
+            )
+            current_move_row = int(current_move.split(" ")[0])
+            current_move_column = int(current_move.split(" ")[1])
+
+        board[current_move_row][current_move_column] = current_player["symbol"]
+        print(current_board(board))
+        # Check if current player won
+        if is_winner(current_player["symbol"]):
+            print(f"Congratulations! {current_player['name']} won!")
+            play_again = (input("Do you want to play again? Y | N\n")).lower()
+            if play_again == "y":
+                game()
+            else:
+                quit()
+        else:
+            # Check if board is full
+            if is_board_full():
+                play_again = input(
+                    """
+                It's a tie!
+                Well done both of you, but we have no winner. 
+                Do you want to play another game? (Y | N)
+                """
+                )
+                if play_again == "Y":
+                    game()
+                else:
+                    quit()
+
+
+game()
